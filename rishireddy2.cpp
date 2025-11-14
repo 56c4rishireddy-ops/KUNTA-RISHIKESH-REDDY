29c75eb30127846ef976749fd597f78ce24e4705
56c4rishireddy-ops <56c4rishireddy@gmail.com>
2025-11-14 11:57:10 +0530

    Add files via upload

---
+++ rishireddy2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int arr[10], max, min, size, i, sum = 0;
+	float avg;
+	
+printf("enter size of the array (<10): ");
+scanf("%d", &size);
+
+printf("enter elements of the array :");
+
+for(i = 0; i< size; i++)
+   {
+   	scan
+   }
+}
